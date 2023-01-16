a9037ed0cb5f0b237aa4c378902e480c6f445adc
Lukas Grinys <lukas.grinys@adeoweb.lt>
2023-01-16 14:25:08 +0200

    (AP-26) [App,ErrorBoundary] Move rendering error handling to ErrorBoundary component

--- src/lib/components/App/useErrorBoundary.tsx
+++ src/lib/components/App/useErrorBoundary.tsx
@@ -21,16 +21,41 @@ export const useErrorBoundary = (WrappedComponent: ReactNode) =>
                     return { renderError };
                 }
 
+                componentDidUpdate() {
+                    if (this.state.renderError) {
+                        this.recoverFromError();
+                    }
+                }
+
+                recoverFromError() {
+                    if (process.env.NODE_ENV === 'development') {
+                        console.log(
+                            'Default window.location.reload() error handler not running in developer mode.'
+                        );
+                        return;
+                    }
+
+                    window.location.reload();
+                }
+
                 render(): ReactNode {
                     const { props, state } = this;
                     const { renderError } = state;
 
+                    /**
+                     * Moved render error handling up here to prevent
+                     * invoking the same errors all over again when
+                     * rendering the very same tree.
+                     *
+                     * Put your desired view for these type of errors
+                     */
+                    if (renderError) {
+                        return <p>Render error has occurred</p>;
+                    }
+
                     return (
                         // @ts-expect-error
-                        <WrappedComponent
-                            {...props}
-                            renderError={renderError}
-                        />
+                        <WrappedComponent {...props} />
                     );
                 }
             },
